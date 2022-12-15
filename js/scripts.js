a7fde750a5372619b483858a4138279aa3bba5ec
JongminKim97 <jmkim@robotics.snu.ac.kr>
2022-12-15 13:54:14 +0900

    add anomaly button + js

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,17 @@
 //     bar.style.width=t+'%'
 //     t++>=72 && clearInterval(barAnimation)
 // },10)
+
+//const getRandom = (min, max) => Math.floor(Math.random()*(max-min)+min);
+//console.log(getRandom(10, 50));
+
+function getRandom(min, max)  {
+    // 결과를 표시할 element
+    const resultElement = document.getElementById('result');
+    
+    // 현재 화면에 표시된 값
+    let number = Math.floor(Math.random()*(max-min)+min);
+        
+    // 결과 출력
+    resultElement.innerText = number;
+  }
